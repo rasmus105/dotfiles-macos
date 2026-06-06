fb29911b6c5a24cc5e13206f51eb5358c7a4d3d5
Rasmus Kargo <rasmus105@protonmail.com>
2026-06-06 21:38:20 +0200

    git: aliases

--- setup.sh
+++ setup.sh
@@ -16,11 +16,12 @@ stow --restow -d "$SCRIPT_DIR" -t ~ home # stow home/ to ~/
 stow --restow -d "$SCRIPT_DIR" -t ~/.local local # stow local/ to ~/.local/
 
 # MacOS configuration stuff
-defaults write com.apple.dock mru-spaces -bool false # avoid MacOS reordering desktops.
 defaults write com.apple.Terminal FocusFollowsMouse -bool true # avoid having to click once to focus on a window
+defaults write com.apple.dock mru-spaces -bool false # avoid MacOS reordering desktops.
 defaults write com.apple.dock autohide -bool true # automatically hide dock.
 defaults write com.apple.dock autohide-time-modifier -float 0.30 # increase animation speed of dock appearing
 defaults write com.apple.dock autohide-delay -float 0 # immediately show dock when moving cursor to the bottom.
+defaults write com.apple.dock show-recents -bool false # don't show recent apps in dock
 
 enable_disk_encryption
 
